bb65b8442c95b9816bf0e793070cf66e6cea1cd0
PandesalPanpan <peterelijahamarticio@iskolarngbayan.pup.edu.ph>
2025-02-11 19:24:30 +0800

    day 16 folder structure, csrf, post route and unguard model/guarded

--- routes/web.php
+++ routes/web.php
@@ -8,16 +8,31 @@ Route::get('/', function () {
 });
 Route::get('/jobs', function () {
 
-    return view('jobs', [
-        'jobs' => Job::with('employer')->paginate(2)
+    return view('jobs.index', [
+        'jobs' => Job::with('employer')->latest()->paginate(3)
     ]);
+});
+
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+});
+
+Route::post('/jobs', function () {
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1,
+    ]);
+    return redirect('/jobs');
 });
 
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
 });
+
+
 
 Route::get('/contact', function () {
     return view('contact');
