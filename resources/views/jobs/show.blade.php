128619440adbfbf2bb66734689bc1745cb0e4f3d
PandesalPanpan <peterelijahamarticio@iskolarngbayan.pup.edu.ph>
2025-02-12 19:03:18 +0800

    day 18 editing, deleting & hidden form, blade methods

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -2,6 +2,10 @@
     <x-slot:heading>
         Job Post
     </x-slot:heading>
-    <p class="font-bold text-lg">{{ $job['title'] }}</p>
-    <p>This job salary is {{ $job['salary'] }}.</p>
+    <p class="font-bold text-lg">{{ $job->title }}</p>
+    <p>This job salary is {{ $job->salary }}.</p>
+
+    <p class="mt-6">
+        <x-button href="/jobs/{{ $job->id }}/edit">Edit</x-button>
+    </p>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use App\Models\Job;
 Route::get('/', function () {
     return view('home');
 });
+
+// Index
 Route::get('/jobs', function () {
 
     return view('jobs.index', [
@@ -13,10 +15,12 @@ Route::get('/jobs', function () {
     ]);
 });
 
+// Create
 Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
 
+// Store
 Route::post('/jobs', function () {
     request()->validate([
         'title' => ['required', 'min:3'],
@@ -30,6 +34,42 @@ Route::post('/jobs', function () {
         'employer_id' => 1,
     ]);
     return redirect('/jobs');
+});
+
+// Edit
+Route::patch('/jobs/{id}', function ($id) {
+    // Validate
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required', 'min:3', 'numeric'],
+    ]);
+
+    // Auth
+
+    // Update
+    $job = Job::findOrFail($id);
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);
+
+    return redirect('/jobs/'.$id);
+});
+
+// Delete
+Route::delete('/jobs/{id}', function ($id) {
+    // Auth
+
+    // Delete
+    $job = Job::findOrFail($id)->delete();
+
+    return redirect('/jobs');
+});
+
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+
+    return view('jobs.edit', ['job' => $job]);
 });
 
 Route::get('/jobs/{id}', function ($id) {
